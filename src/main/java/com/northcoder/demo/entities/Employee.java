0c90d5724a6bf02074947a68c74327238d5efcf1
northCoder <northcoder.root@gmail.com>
2022-07-28 14:00:22 -0400

    Removed unused code.
    Removed unused comparable code from employee record.

--- src/main/java/com/northcoder/demo/entities/Employee.java
+++ src/main/java/com/northcoder/demo/entities/Employee.java
@@ -8,24 +8,6 @@ public record Employee(
         String office,
         int age,
         LocalDate startDate,
-        String salary)
-        implements Comparable<Employee> {
-
-    @Override
-    public int compareTo(Employee other) {
-        if (other == null || other.name == null) {
-            return 1;
-        }
-        // just a crude demo of sorting on last name before 1st name,
-        // which assumes there always is a last name and a 1st name!
-        String[] thisNames = this.name.split(" ");
-        String[] otherNames = other.name.split(" ");
-        int i = thisNames[1].compareTo(otherNames[1]);
-        if (i != 0) {
-            return i;
-        } else {
-            return thisNames[0].compareTo(otherNames[0]);
-        }
-    }
+        String salary) {
 
 }
